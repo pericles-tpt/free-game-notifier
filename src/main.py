2cb4f46f0152b6de2f137be1c5b7c12bc21bb4ea
Peri <pericles@outlook.com.au>
2022-01-04 06:34:03 +0000

    Refactor

--- src/main.py
+++ src/main.py
@@ -22,83 +22,69 @@ from api  import attempt_get_raw_html, attempt_po_api_request
 from time import sleep
 
 MINUTE      = 60
-RUN_EVERY   = 15 * MINUTE
+run_every   = 15 * MINUTE
 found_deals = []
 
 def main():
 	global found_deals
 
 	while(1):
-		# 1. Get raw html
-		data_free    = attempt_get_raw_html("https://gg.deals/au/news/freebies")
-		data_bundles = attempt_get_raw_html("https://gg.deals/au/news/bundles")
+		# 1. Get valid items from raw html
+		bundles  = filter_valid_items("https://gg.deals/au/news/bundles")
+		freebies = filter_valid_items("https://gg.deals/au/news/freebies")
 
-		# 2. Go through data_free char by char (from end to beginning)
-		e_str     = "</a></h3>"
-		e_str_len = len(e_str)
-		s_str     = ">"
+		# 2. Reset the notification queue
 		notify_q  = []
-		notify_q2 = []
 
-		for i in range(e_str_len, len(data_free)):
+		# 3. Update notify_q and found_deals
+		notify_q    += freebies + bundles
+		found_deals += freebies + bundles
+	
+		# 4. Reverse notify_q to present notifications in the right order
+		notify_q.reverse()
+		for i in notify_q:
+			attempt_po_api_request(f"{i[1]}: '{i[0]}'")
 
-			# If a match is found with the end string...
-			if (data_free[i-e_str_len:i] == e_str):
+		sleep(run_every)
 
-				# Keep going back until a match with s_str is found...
-				c = ""
-				j = 0
-				while (c != s_str):
-					c = data_free[i - e_str_len - j]
-					j += 1
+def filter_valid_items(url, s_str=">", e_str="</a></h3>"):
+	e_str_len = len(e_str)
+	data      = attempt_get_raw_html(url)
+	ret       = []
 
-				# Determine if it's the desired item... (could put some filters here) 
-				item_str = data_free[i-e_str_len-j+2: i-e_str_len]
+	for i in range(e_str_len, len(data)):
+
+		# If a match is found with the end string...
+		if (data[i - e_str_len:i] == e_str):
+
+			# Keep going back until a match with s_str is found...
+			c = ""
+			j = 0
+			while (c != s_str):
+				c = data[i - e_str_len - j]
+				j += 1
+
+			# Determine if it's the desired item... (could put some filters here)
+			tup      = None
+			item_str = data[i- e_str_len - j+2: i - e_str_len]
+			if ("freebies" in url):
 				if (item_str[0:4] == "FREE" and "on" in item_str):
-					lst   = item_str[5:].split(' on ')			
+					lst   = item_str[5:].split(' on ')		
 					game  = lst[0]
 					store = lst[1]
 					if (lst[1][:4] == "the "):
 						store = lst[1][4:]
 					tup = (game, store)
-
-					# Send a notification if it hasn't already been found
-					if (tup not in found_deals):
-						notify_q.append(tup)
-						found_deals.append(tup)
-
-		for i in range(e_str_len, len(data_bundles)):
-
-			# If a match is found with the end string...
-			if (data_bundles[i-e_str_len:i] == e_str):
-
-				# Keep going back until a match with s_str is found...
-				c = ""
-				j = 0
-				while (c != s_str):
-					c = data_bundles[i - e_str_len - j]
-					j += 1
-
-				# Determine if it's the desired item... (could put some filters here) 
-				item_str = data_bundles[i- e_str_len - j+2: i - e_str_len]			
+			elif ("bundles" in url):
 				game  = item_str
 				store = 'BUNDLE'
 				tup = (game, store)
 
-				# Send a notification if it hasn't already been found
-				if (tup not in found_deals):
-					notify_q2.append(tup)
-					found_deals.append(tup)
+			# Send a notification if it hasn't already been found
+			if ((tup != None) and (tup not in found_deals)):
+				ret.append(tup)
 	
-		# This reverse() is specific to gg.deals, to get the notifications in the right order
-		notify_q.reverse()
-		notify_q2.reverse()
-		for i in notify_q:
-			attempt_po_api_request(f"{i[1]}: '{i[0]}'")
-		for i in notify_q2:
-			attempt_po_api_request(f"{i[1]}: '{i[0]}'")
-
-		sleep(RUN_EVERY)
+	return ret
 
 if __name__ == "__main__":
 	main()
