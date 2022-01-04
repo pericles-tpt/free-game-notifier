3c0ee245e423668f30771159e28887d94e9f922a
pericles@outlook.com.au <pericles@outlook.com.au>
2022-01-04 14:56:16 +1100

    Added requirements.txt and MIT licence header

--- src/api.py
+++ src/api.py
@@ -1,3 +1,23 @@
+# Copyright (c) 2022 Pericles Telemachou
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+# OR OTHER DEALINGS IN THE SOFTWARE.
+
 import http.client, urllib.request, requests
 from files          import retrieve_from_yaml
 from datetime       import datetime
@@ -45,7 +65,6 @@ def attempt_po_api_request(message):
 def attempt_get_raw_html(url):
     timeouts_count  = 0
     sc              = 0
-    timeout         = 30
     
     # Keep sending the request until we get a status = 200        
     time_start = datetime.now()
